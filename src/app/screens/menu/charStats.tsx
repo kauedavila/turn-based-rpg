78885bccacc059f48b3babc924898abb1cd8bed7
kauedavila <kaueourives2@gmail.com>
2024-03-15 08:49:00 -0300

    fix: reposicionando textos em charStats

--- src/app/screens/menu/charStats.tsx
+++ src/app/screens/menu/charStats.tsx
@@ -3,14 +3,14 @@ const CharStats = ({ character }: any) => {
     <>
       <div
         className="flex flex-col 
-                items-start justify-start w-full h-auto pb-2 text-sm"
+                items-start justify-start w-full h-full pb-2 text-sm"
       >
         <p>
           <strong className="text-base">{character?.name}</strong>
         </p>
-        <ul>
-          <li>LV {character?.level}</li>
+        <ul className="text-sm">
           <li>
+            <span>LV {character?.level} - </span>
             EXP {character?.experience} / {character?.level * 20}
           </li>
           <li>HP {character?.health}</li>
